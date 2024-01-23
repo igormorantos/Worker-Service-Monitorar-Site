b8d475e053d5d1b32b8b5967ce20d4bc38871de8
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-23 11:57:36 -0300

    "implementação dos contratos"

---
+++ services/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkerService2.contracts;
+using WorkerService2.utils;
+
+namespace WorkerService2.services
+{
+    public class Email : IEmail
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            var outlook = new EmailHelper("smtp.office365.com", "emailDeEnvioAqui", "SenhaDoEmailAq");
+            outlook.SendEmail(new List<string> { to }, subject, body, new());
+        }
+    }
+}
---
+++ services/HttpService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using WorkerService2.contracts;
+
+namespace WorkerService2.services
+{
+    public class HttpService : IHttpService
+    {
+        public async Task<HttpStatusCode> CheckStatusSite(string url)
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetAsync(url);
+                return response.StatusCode;
+            }
+            catch (HttpRequestException)
+            {
+                return HttpStatusCode.NotFound;
+            }
+        }
+    }
+}
