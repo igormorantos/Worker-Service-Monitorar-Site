0b9075b92833385ca5043c6a2d206c848fe600fc
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2024-01-23 11:55:03 -0300

    "interfaces: IEmail/IHttpService"

---
+++ contracts/IHttpService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerService2.contracts
+{
+    public interface IHttpService
+    {
+        Task<HttpStatusCode> CheckStatusSite(string url);
+    }
+}
